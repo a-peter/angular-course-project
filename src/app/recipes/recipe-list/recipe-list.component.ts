661296a39e79fd84c388bdee314461d1ed3188fa
Anselm Peter <anselm.peter@iav.de>
2020-04-20 11:27:18 +0200

    087 - passing from list to details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -12,10 +12,19 @@ export class RecipeListComponent implements OnInit {
     new Recipe('Reispfanne', 'Alles mixen und kochen.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png')
   ];
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
+  selectedRecipe: Recipe = this.recipes[0];
+
   constructor() { 
   }
 
   ngOnInit(): void {
   }
 
+  doIt(recipe: Recipe) {
+    console.log("recipe clicked", recipe.name);
+    this.selectedRecipe = recipe;
+    this.recipeSelected.emit(recipe);
+  }
 }
