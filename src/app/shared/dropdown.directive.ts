7f6a13183053e5f4646086088d1249c31af5c2fd
Anselm Peter <anselm.peter@iav.de>
2020-04-21 13:48:27 +0200

    103 - close dropdown by clicking anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -8,12 +8,21 @@ export class DropdownDirective {
 
     constructor(private elementRef: ElementRef) {}
 
-    @HostListener('click') toggleOpen() {
-        // console.log('DropDown clicked', eventData);
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        // console.log('DropDown clicked', event);
         // console.log(this.elementRef);
         // console.dir(this.elementRef.nativeElement);
         // console.log(this.elementRef.nativeElement.classList);
+
+        // This code, to close the DropDown by clicking
+        // the DropDown exactly
         // this.elementRef.nativeElement.classList.toggle('open');
-        this.isOpen = !this.isOpen;
+        // this.isOpen = !this.isOpen;
+
+        // This code, to close the DropDown on a click
+        // anywhere on the page.
+        console.log(this.elementRef.nativeElement);
+        console.log(event.target);
+        this.isOpen = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
 }
