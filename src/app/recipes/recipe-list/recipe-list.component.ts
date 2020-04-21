663b390b5022052e6df8540e16a13f0fcd8a4598
Anselm Peter <anselm.peter@iav.de>
2020-04-21 13:34:20 +0200

    102 - Angular solution

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Eierkuchen', 'Alles mixen und in der Pfanne backen.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png'),
-    new Recipe('Reispfanne', 'Alles mixen und kochen.', 'https://www.typescriptlang.org/icons/icon-96x96.png?v=e0cca9b778c3248c7434bc3c68c0e8b2')
+    new Recipe('Reispfanne', 'Alles mixen und kochen.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/900px-JavaScript-logo.png')
   ];
 
   @Output() recipeSelectedComponent = new EventEmitter<Recipe>();
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,17 +1,19 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef, HostBinding } from '@angular/core';
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective {
-    
+    @HostBinding('class.open') isOpen = false;
+
     constructor(private elementRef: ElementRef) {}
 
-    @HostListener('click') onClick(eventData: Event) {
-        console.log('DropDown clicked', eventData);
-        console.log(this.elementRef);
-        console.dir(this.elementRef.nativeElement);
-        console.log(this.elementRef.nativeElement.classList);
-        this.elementRef.nativeElement.classList.toggle('open');
+    @HostListener('click') toggleOpen() {
+        // console.log('DropDown clicked', eventData);
+        // console.log(this.elementRef);
+        // console.dir(this.elementRef.nativeElement);
+        // console.log(this.elementRef.nativeElement.classList);
+        // this.elementRef.nativeElement.classList.toggle('open');
+        this.isOpen = !this.isOpen;
     }
 }
