7820510a121bc3eb5759ca897fee2eca749d2316
Anselm Peter <anselm.peter@iav.de>
2020-05-15 10:16:12 +0200

    218 - adding validation

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,25 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
 
 @Component({
-    selector: 'app-shopping-edit',
-    templateUrl: './shopping-edit.component.html',
-    styleUrls: ['./shopping-edit.component.css']
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
-    name = 'ShoppingEditComponent';
+  name = 'ShoppingEditComponent';
+  @ViewChild('f', { static: false }) amountForm: NgForm;
+  // @Output() ingredientAdded = new EventEmitter<Ingredient>(); // oder:
+  // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
 
-    // @Output() ingredientAdded = new EventEmitter<Ingredient>(); // oder:
-    // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  constructor(private shoppingList: ShoppingListService) {}
 
-    constructor(private shoppingList: ShoppingListService) {}
+  onAddItem(form: NgForm) {
+    const formValues = form.value;
+    const newIngredient = new Ingredient(formValues.name, formValues.amount);
+    console.log('shopping-edit: Add item', newIngredient);
+    this.shoppingList.addIngredient(newIngredient);
+  }
 
-    onAddItem(form: NgForm) {
-        const formValues = form.value ;
-        const newIngredient = new Ingredient(formValues.name, formValues.amount);
-        console.log('shopping-edit: Add item', newIngredient);
-        this.shoppingList.addIngredient(newIngredient);
-    }
+  logForm() {
+    console.log(this.amountForm);
+  }
 }
