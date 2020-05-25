601cb2c216fe0aaac9448594a4c3cfc48db7b759
Anselm Peter <anselm.peter@iav.de>
2020-05-25 13:49:14 +0200

    282 - fixing empty arrays

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,21 +9,22 @@ import { Subject } from 'rxjs';
 })
 export class RecipeService {
   recipeListChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Eierkuchen',
-      'Alles mixen und in der Pfanne backen.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png',
-      [new Ingredient('Eier', 2), { name: 'Mehl', amount: 250 }]
-    ),
-    new Recipe(
-      'Reispfanne',
-      'Alles mixen und kochen.',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/900px-JavaScript-logo.png',
-      [new Ingredient('Reis', 200), { name: 'Hackfleisch', amount: 300 }]
-    ),
-  ];
+  
+  private recipes: Recipe[] = [];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Eierkuchen',
+  //     'Alles mixen und in der Pfanne backen.',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/768px-Google_%22G%22_Logo.svg.png',
+  //     [new Ingredient('Eier', 2), { name: 'Mehl', amount: 250 }]
+  //   ),
+  //   new Recipe(
+  //     'Reispfanne',
+  //     'Alles mixen und kochen.',
+  //     'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/900px-JavaScript-logo.png',
+  //     [new Ingredient('Reis', 200), { name: 'Hackfleisch', amount: 300 }]
+  //   ),
+  // ];
 
   constructor(private shoppingList: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { Ingredient } from './ingredient.model';
 
 @Injectable({
   providedIn: 'root',
@@ -26,10 +28,19 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>(
+    this.http
+    .get<Recipe[]>(
       this.recipesUrl,
       {params: new HttpParams().set('print', 'pretty')}
-    ).subscribe(
+    )
+    .pipe(map(
+      recipes => {
+        return recipes.map(recipe => {
+          return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+        })
+      }
+    ))
+    .subscribe(
       recipes => {
         console.log('Fetched', recipes);
         this.recipeService.setRecipes(recipes);
