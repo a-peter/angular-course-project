b591b087f7a9817cebba93254c2cc212e1dc6214
Anselm Peter <anselm.peter@iav.de>
2020-05-26 08:22:38 +0200

    289 - handling from input

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
+  isLoginMode = true;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form.value);
+    form.reset();
+  }
+  
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
