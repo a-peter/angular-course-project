1e94fd8903fba7b7566d935eb5cd197655153267
Anselm Peter <anselm.peter@iav.de>
2020-05-26 14:36:48 +0200

    300 - adding token to get request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { Ingredient } from './ingredient.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -12,41 +12,48 @@ export class DataStorageService {
   private serverRoot = 'https://ng-course-recipe-book-82868.firebaseio.com/';
   private recipesUrl = this.serverRoot + 'recipes.json';
 
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private authService: AuthService
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
 
-    this.http.put(this.recipesUrl, recipes).subscribe((response) => {
-      console.log(response);
-    });
+    this.http
+      .put(this.recipesUrl, recipes, {
+        params: new HttpParams().set('auth', ''),
+      })
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   fetchRecipes() {
-    return this.http
-      .get<Recipe[]>(this.recipesUrl, {
-        params: new HttpParams().set('print', 'pretty'),
-      })
-      .pipe(
-        map((recipes) => {
-          return recipes.map((recipe) => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
-            };
-          });
-        }),
-        tap(recipes => 
-          this.recipeService.setRecipes(recipes))
-      )
-      // .subscribe(
-      //   (recipes) => {
-      //     console.log('Fetched', recipes);
-      //     this.recipeService.setRecipes(recipes);
-      //   },
-      //   (error) => {
-      //     console.error('Error', error);
-      //   }
-      // );
+    return this.authService.user.pipe(
+      take(1),
+      // This passes the internally returned observable to
+      // the outer return statement.
+      //
+      exhaustMap((user) => {
+        console.log('token', user.token);
+
+        return this.http.get<Recipe[]>(this.recipesUrl, {
+          params: new HttpParams()
+            .set('print', 'pretty')
+            .append('auth', user.token),
+        });
+      }),
+      map((recipes) => {
+        return recipes.map((recipe) => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : [],
+          };
+        });
+      }),
+      tap((recipes) => this.recipeService.setRecipes(recipes))
+    );
   }
 }
